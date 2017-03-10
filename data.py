3b0f8d881656c6562a033df493597a1e289f5fac
Zhanpeng Liang <hectorilles@gmail.com>
2017-03-10 13:27:29 +0900

    basically works

--- data.py
+++ data.py
@@ -44,6 +44,7 @@ class Synthesis(object):
 			toprint, skline = self.comps[f].sketch(var_gen, counter) # sketch() return a tuple
 			sketch.append('\t' + (toprint) + ' in')
 			sketch_ads.add_line(skline)
+			print('    ' + str(skline))
 		return_holes = [next(counter) for _ in range(self.targetfunc.output_len())]
 		sketch.append('in #' + ', '.join(map(str, return_holes)))
 		sketch_ads.add_return_line(self.targetfunc, return_holes)
@@ -163,6 +164,8 @@ class SketchLine(object):
 	def __init__(self, holes, variables):
 		self._holes = holes # list of (hole#, type)
 		self._vars = variables # list of (var_name, type)
+	def __str__(self):
+		return '(holes: ' + str(self._holes) + ', vars: ' + str(self._vars) + ')'
 	def holes(self):
 		return (hole for hole in self._holes)
 	def variables(self):
@@ -181,7 +184,10 @@ class Sketch(object):
 		for typing in self.type_vars:
 			for var in self.type_vars[typing]:
 				yield (var, typing)
-
+	def _hypos(self):
+		for hole_cands in self.hypos.values():
+			for hyp in hole_cands.values():
+				yield hyp
 	def add_signature(self, sigtr):
 		'''Simply add parameters(variables) into types, and set up variable constraints frame'''
 		assert isinstance(sigtr, Signature)
@@ -214,7 +220,7 @@ class Sketch(object):
 		assert isinstance(sigtr, Signature)
 		for hole, typing in zip(holes, sigtr.rtypes):
 			self.hole_vars[hole] = self.type_vars[typing].copy()
-			if hole not in self.type_holes:
+			if typing not in self.type_holes:
 				self.type_holes[typing] = set()
 			self.type_holes[typing].add(hole)
 
@@ -234,28 +240,39 @@ class Sketch(object):
 			self.hypos[hole] = {}
 			for var in self.hole_vars[hole]:
 				self.hypos[hole][var] = z3.Bool(hypo_var_gen(hole, var))
-				print(self.hypos[hole][var])
 			vcandlist = list(self.hypos[hole].values())
-			print('set up constraint: ' + str(vcandlist))
+			print('HOLE CONSTRAINTS LIST: ' + str(vcandlist))
 			self.s.add(z3.AtMost(vcandlist + [1])) # all add up to 0 or 1
 			self.s.add(z3.Or(vcandlist))
 
 		for var in self.var_holes:
 			hcandlist = list(self.hypos[hole][var] for hole in self.var_holes[var])
+			print('VAR CONSTRAINTS LIST ' + str(hcandlist))
 			self.s.add(z3.AtLeast(hcandlist + [1]))
 	
+	def _process_model(self):
+		m = self.s.model()
+		block = []
+		for hyp in self._hypos():
+			if z3.is_true(m.eval(hyp)):
+				block.append(hyp)
+		self.s.add(z3.Not(z3.And(block)))
+		return m
+
 	def enum_subst(self):
 		'''enumerate possible completions of the code sketch'''
-		print('Sketch.enum_subst: ')
-		print('type_vars: ' + str(self.type_vars))
-		print('type_holes: ' + str(self.type_holes))
-		print('hole_vars: ' + str(self.hole_vars))
-		print('var_holes: ' + str(self.var_holes))
 		self._add_var_cands()
 		self._set_up_constraints()
+		if DEBUG:
+			print('Sketch.enum_subst: ')
+			print('type_vars: ' + str(self.type_vars))
+			print('type_holes: ' + str(self.type_holes))
+			print('hole_vars: ' + str(self.hole_vars))
+			print('var_holes: ' + str(self.var_holes))
+			print()
 		while z3.sat == self.s.check():
-			print(self.s.model())
-			break
+			print('  -----',end='')
+			print(self._process_model())
 
 	def gen_hypothesis(self):
 		pass
