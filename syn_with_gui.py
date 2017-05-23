b01aaabf107c2f0777ce20bd12c5dc5ac87e3de0
Zhanpeng Liang <hectorilles@gmail.com>
2017-05-23 10:23:28 +0900

    rough intermediate results

--- syn_with_gui.py
+++ syn_with_gui.py
@@ -5,13 +5,27 @@ from os import mkdir
 class FiledError(Exception):
 	pass
 
+def decode_string(string):
+	temp = string.strip(' \'\"')
+	return bytes(temp, "utf-8").decode("unicode_escape")
+
+def select_from_list(lst, selector):
+	''' note: this function return a map object '''
+	assert isinstance(lst, list) and isinstance(selector, list)
+	return map(lst.__getitem__, selector)
+
 class App(Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
+		self.sigtr_dict = None
 		self.test_entries = []
 		self.active_tests = 0
-		self.pack()
+		self.max_active = 10
+		self.midtest_entries = []
 
+		self.midtestframe = None
+
+		self.pack()
 		self.create_left_panel()
 		self.create_right_panel()
 
@@ -65,20 +79,20 @@ class App(Frame):
 
 	def create_right_panel(self):
 		self.rightframe = Frame(self, width=380, height=550)
-		self.rightframe.pack(side='right')
+		self.rightframe.pack(side='left')
 		self.rightframe.pack_propagate(0)
 
 		self.testframe = LabelFrame(self.rightframe, text='Test Cases', width=30, height=500)
 		self.testframe.pack()
 
-		test_example = Label(self.testframe, text='stutter [1;2] = [1;1;2;2]')
+		test_example = Label(self.testframe, text='Example: stutter [1;2] = [1;1;2;2]')
 		# test_example.grid(row=0, column=0)
 		test_example.pack(side='top')
 
 		def add_test():
 			if self.active_tests < len(self.test_entries):
 				self.test_entries[self.active_tests][0].pack(side='top')
-			elif self.active_tests < 10:
+			elif self.active_tests < self.max_active:
 				self.test_entries.append((Entry(self.testframe, width=300), StringVar()))
 				ent, content = self.test_entries[-1]
 				ent['textvariable'] = content
@@ -141,29 +155,93 @@ class App(Frame):
 					list2string(sigtr['libdirs']) if isinstance(sigtr['libdirs'], list) else sigtr['libdirs'])
 			except Exception as e:
 				print(e)
+			with open(sigtr['testpath'], 'r') as testfile:
+				testcases = re.findall(r'try\((.*?)\)', testfile.read()) # match test wrapped by "try()"
+				for _ in range(self.active_tests - len(testcases)):
+					delete_test()
+				for i, testcase in enumerate(testcases):
+					if i > self.max_active - 1:
+						return
+					if self.active_tests <= i:
+						add_test()
+					set_text(self.test_entries[i][0], testcase)
 		import_button = Button(importframe, text='Import json file', command=import_json)
 		import_button.grid(row=0, column=1)
 
+		midtest_button = Button(self.rightframe, text='Add middle tests', command=self.create_mid_test_panel)
+		midtest_button.pack(side='bottom')
+
+		syn_part_button = Button(self.rightframe, text='Syn Part', command=self.synthesize_nested_function)
+		syn_part_button.pack(side='bottom')
+
+	def create_mid_test_panel(self):
+		if self.midtestframe:
+			return
+		self.midtestframe = LabelFrame(self, text='Middle tests', width=320, height=300)
+		self.midtestframe.pack(side='top')
+
+		examplelb = Label(self.midtestframe, text='Examples: 0; 2; "10/2"')
+		examplelb.pack(side='top')
+
+		argspecframe = Frame(self.midtestframe)
+		argspecframe.pack(side='top')
+		argspeclb = Label(argspecframe, text='arg#:')
+		argspeclb.grid(row=0, column=0)
+		self.argspec_entry = Entry(argspecframe)
+		self.argspec_entry.insert(0, 'specify argument number')
+		self.argspec_entry.grid(row=0, column=1)
+
+		for _ in range(self.max_active):
+			self.midtest_entries.append(Entry(self.midtestframe))
+			self.midtest_entries[-1].pack(side='top')
+
+	def read_sigt_info(self):
+		self.sigtr_dict = {}
+		extract_list = lambda lst : list(map(str.strip, lst.split(';')))
+		self.sigtr_dict['name'] = self.name_entry.get().strip()
+		self.sigtr_dict['paramNames'] = extract_list(self.para_entry.get())
+		self.sigtr_dict['paramTypes'] = extract_list(self.para_type_entry.get())
+		self.sigtr_dict['libdirs'] = extract_list(self.libs_entry.get())
+		self.sigtr_dict['tgtTypes'] = extract_list(self.return_type_entry.get())
+		print(self.sigtr_dict)
+		# check valid synthesis parameter
+		if any([e == '' or e == [''] for e in self.sigtr_dict.values()]):
+			self.errmsg.set('Some of the fields are empty')
+			raise FiledError()
+		if len(self.sigtr_dict['paramNames']) != len(self.sigtr_dict['paramTypes']):
+			self.errmsg.set('Lengths of parameters and types do not agree')
+			raise FiledError()
+
+
+
+	def get_inputs(self, selector):	
+		selected = select_from_list(self.test_entries, selector)
+		io_series = (e[0].get().split('=') for e in selected)
+		lhs, rhs = [], []
+		for l, _ in io_series:
+			lhs.append(list(map(decode_string, l.split()[1:]))) # BUG: space in string!!!
+			# rhs.append(r.strip())
+		return lhs
+
+	def synthesize_nested_function(self):
+		if not self.sigtr_dict:
+			self.read_sigt_info()
+		try:
+			spec_args = list(map(lambda x:int(x.strip()), self.argspec_entry.get().split(';')))
+			midtest_inputs = [decode_string(e.get()) for e in self.midtest_entries]
+			effective_list = [i for i in range(self.max_active) if midtest_inputs[i] != '']
+			middle_results = list(select_from_list(midtest_inputs, effective_list))
+		except Exception as e:
+			print(e)
+			return
+		selected_inputs = self.get_inputs(effective_list)
+		# select horizontally
+		selected_argslist = [list(select_from_list(arglist, spec_args)) for arglist in selected_inputs]
+		print(selected_argslist)
 
 	def start_synthesis(self):
-		self.sigtr_dict = {}
-		def read_sigt_info():
-			extract_list = lambda lst : list(map(str.strip, lst.split(';')))
-			self.sigtr_dict['name'] = self.name_entry.get().strip()
-			self.sigtr_dict['paramNames'] = extract_list(self.para_entry.get())
-			self.sigtr_dict['paramTypes'] = extract_list(self.para_type_entry.get())
-			self.sigtr_dict['libdirs'] = extract_list(self.libs_entry.get())
-			self.sigtr_dict['tgtTypes'] = extract_list(self.return_type_entry.get())
-			print(self.sigtr_dict)
-			# check valid synthesis parameter
-			if any([e == '' for e in self.sigtr_dict.values()]):
-				self.errmsg.set('Some of the fields are empty')
-				raise FiledError()
-			if len(self.sigtr_dict['paramNames']) != len(self.sigtr_dict['paramTypes']):
-				self.errmsg.set('Lengths of parameters and types do not agree')
-				raise FiledError()
 		try:
-			read_sigt_info()
+			self.read_sigt_info()
 		except FiledError:
 			return
 		folder = 'test'
@@ -190,6 +268,7 @@ class App(Frame):
 		score = 'json/scores.json'
 		synt = Synthesis(sigtr_dict=self.sigtr_dict, func_scores=score)
 		synt.setup()
+		print('Program length:', self.syn_len)
 		synt.set_syn_len(self.syn_len)
 		try:
 			solution = synt.start()
@@ -202,6 +281,6 @@ class App(Frame):
 				self.codeview.insert(END, line + '\n')
 
 root = Tk('OCaml Program Synthesis')
-root.geometry("900x600")
+root.geometry("1100x600")
 app = App(master=root)
 app.mainloop()
